4697b9d2c17add4ef1150e7551ddf914fc70d7bc
KINOMOTO, Masakazu <kino2718@gmail.com>
2025-06-26 14:39:29 +0900

    SearchScreenをリファクタリング。

--- app/src/main/java/net/kino2718/podcast/ui/search/SearchScreen.kt
+++ app/src/main/java/net/kino2718/podcast/ui/search/SearchScreen.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -44,7 +45,7 @@ fun SearchScreen(
     modifier: Modifier = Modifier,
     viewModel: SearchViewModel = viewModel()
 ) {
-    var query by remember { mutableStateOf("") }
+    var query by rememberSaveable { mutableStateOf("") }
     val uiState by viewModel.searchUIStateFlow.collectAsState()
     val subscribed by viewModel.subscribedFlow.collectAsState()
     val keyboardController = LocalSoftwareKeyboardController.current
